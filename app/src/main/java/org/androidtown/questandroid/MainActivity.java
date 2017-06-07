48a4114faffa3ac00d06a5dfae4b60c4b2bb94a7
op7267 <op7267@korea.ac.kr>
2017-06-07 17:10:01 +0900

    Initial commit

---
+++ app/src/main/java/org/androidtown/questandroid/MainActivity.java
@@ -0,0 +1,17 @@
+package org.androidtown.questandroid;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+/**
+ * Created by Jin on 2017-06-07.
+ */
+
+public class MainActivity extends Activity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
